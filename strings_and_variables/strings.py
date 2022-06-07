29c0f97580a18085a7a8a1f325d3a0032d858aa3
quintincook <105319336+quintincook@users.noreply.github.com>
2022-06-07 10:19:40 -0400

    name

---
+++ strings_and_variables/strings.py
@@ -0,0 +1,13 @@
+#if you want to use text in python you have to use a string
+print("you can use double quotations")
+print('or single quotations')
+
+#some characters cant be used in a string like if you want to use a double quotation in a double quotation 
+# so you use a backlash:
+print('Brian\'s mother: He\'s not an angel.')
+
+#another ex
+print('I\'m a programmer!... kinda')
+
+#or: 
+print("He said:\" I'm learning!\"")
