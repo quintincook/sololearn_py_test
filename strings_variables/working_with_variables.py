a6586c5d5331dfe5e1982b72c81a0affe1a61218
Yash-ShahBG <yash.shah@berkshiregrey.com>
2022-06-10 14:11:49 -0400

    add new learning files

---
+++ strings_variables/working_with_variables.py
@@ -0,0 +1,16 @@
+#you can use variables to perforom corrisponding operations, just as you would with numbers and strings 
+x = 7 
+print(x)
+
+print(x + 3)
+print(x)
+
+#variables can be reassigned as many times as you want, in order change their value. 
+# in Python, variables dont have specific types, so you can assign a string to a variable, and later assign an integer to the same variable 
+x = 123.456
+print(x)
+
+x = "this is a string"
+print(x + "!")
+#changing vaiables is possible but it is not good practice. so to avoid mistkaes, try to avoid overwriting the smae variables with different data types 
+
