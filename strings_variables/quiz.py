dca16f1e7a7ef6f454be04e0cdc230ac868bedcd
quintincook <105319336+quintincook@users.noreply.github.com>
2022-06-13 12:04:21 -0400

    more lessons

---
+++ strings_variables/quiz.py
@@ -0,0 +1,20 @@
+spam = "7"
+spam = spam + "0"
+eggs = int(spam)+ 3
+print(float(eggs))
+
+age = int(input())
+print(age+8)
+
+x = 5
+y = x + 3
+y = int(str(y) + "2")
+print(y)
+
+x = 4
+x += 5
+print(x)
+
+x = 3 
+num = 17
+print(num%x)
